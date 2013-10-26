920ddf55451e3d402a61bd232c542b458f687bb7
Shockk <shokku.ra@gmail.com>
2013-10-26 03:39:58 +0100

    Wrote polymorphic fib functions.

--- fib.hs
+++ fib.hs
@@ -1,10 +1,18 @@
 fib :: Int -> Int
 fib n = fibGen 0 1 n
 
-fibs :: [Int]
+fibs :: [Integer]
 fibs = 0 : 1 : [ a + b | (a, b) <- zip fibs (tail fibs)]
 
 fibGen :: Int -> Int -> Int -> Int
 fibGen a b n = case n of
     0 -> a
     n -> fibGen b (a + b) (n - 1)
+
+fibPoly :: (Num a, Eq a, Num b, Eq b) => a -> b
+fibPoly n = fibGenPoly 0 1 n
+
+fibGenPoly :: (Num a, Eq a, Num b, Eq b) => b -> b -> a -> b
+fibGenPoly a b n = case n of
+    0 -> a
+    n -> fibGenPoly b (a + b) (n - 1)
