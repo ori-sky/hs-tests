d1b3a0f94076159221b048b26bb8aebab128de9c
Shockk <shokku.ra@gmail.com>
2013-10-26 18:03:15 +0100

    Rewrote fibGenPoly as pattern matched definitions rather than single definition using `case of`.

--- fib.hs
+++ fib.hs
@@ -13,6 +13,5 @@ fibPoly :: (Num a, Eq a, Num b, Eq b) => a -> b
 fibPoly n = fibGenPoly 0 1 n
 
 fibGenPoly :: (Num a, Eq a, Num b, Eq b) => b -> b -> a -> b
-fibGenPoly a b n = case n of
-    0 -> a
-    n -> fibGenPoly b (a + b) (n - 1)
+fibGenPoly a b 0 = a
+fibGenPoly a b n = fibGenPoly b (a + b) (n - 1)
